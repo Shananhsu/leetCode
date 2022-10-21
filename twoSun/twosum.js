42f6536a1180ebffb9c104e4b361f5f341bfccb9
shananhsu <shananhsu@gmail.com>
2022-10-21 11:44:20 +0800

    twosum

---
+++ twoSun/twosum.js
@@ -0,0 +1,40 @@
+// ---------暴力破解---------
+// let arr = [2, 7, 11, 15];
+// let ans = 9;
+
+// var twoSum = function () {
+
+//     for (let i = 0; i < arr.length; ++i) {
+//         for (let j = i + 1; j < arr.length; ++j) {
+//             if (arr[i] + arr[j] === ans) {
+//                 return console.log(i, j)
+//             }
+//         }
+//     }
+// }
+
+// twoSum();
+// ---------暴力破解---------
+
+
+
+// ---------new Map()---------
+let nums = [3, 3];
+let target = 6;
+
+var twoSum = function (nums, target) {
+    const mymap = new Map();
+    for (let i = 0; i < nums.length; ++i) {
+        let arrDetail = nums[i];
+        let subtraction = target - arrDetail;
+        if (mymap.has(subtraction)) {
+            return [mymap.get(subtraction) , i]
+        }
+        mymap.set(arrDetail, i);
+    }
+}
+console.log(twoSum(nums, target))
+
+// ---------new Map()---------
+
+
