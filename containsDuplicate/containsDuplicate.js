9aff4684afe455e99409bf61b9072767ba6b9c6a
shananhsu <shananhsu@gmail.com>
2022-10-28 16:27:48 +0800

    可以在優化

---
+++ containsDuplicate/containsDuplicate.js
@@ -0,0 +1,17 @@
+nums = [1, 2, 3, 1]
+
+var containsDuplicate = function (nums) {
+
+    let res = nums.filter((el, index) => {
+        // console.log(nums.el);
+        console.log(index);
+        return nums.indexOf(el) !== index
+    })
+    if (res.length === 0) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+containsDuplicate(nums);
