e1d346c36eaa236dc682ab4522a1457dcae2a641
shananhsu <shananhsu@me.com>
2024-04-19 10:12:55 +0800

    新增   第 3110 題

--- 3110_ScoreOfAString/index.js
+++ 3110_ScoreOfAString/index.js
@@ -3,11 +3,8 @@
 var scoreOfString = function (s) {
     let ans = 0;
     for (let i = 1; i < s.length; i++) {
-        // console.log(s.charCodeAt(i))
-        // console.log(s.charCodeAt(i - 1))
         ans = ans + Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
     }
-    console.log(ans);
     return ans;
 };
 
